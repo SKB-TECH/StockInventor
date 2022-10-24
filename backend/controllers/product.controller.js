69ba680760d567d82abf50bc164d83454462f729
malo-dev <leadermushio377@gmail.com>
2022-10-24 17:15:38 +0200

    Add deleteProduct and createProduct function

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,11 +1,34 @@
+const { Pool, Client } = require('pg')
+const pool = new Pool({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+})
 
-exports.Main = (req, res, next) => {
-    res.render('main')
-}
+const client = new Client({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+})
 
 
-exports.createProduct = (req, res, next) => {
 
+
+
+exports.createProduct = (req, res) => {
+ try {
+		pool.connect(async (error, client, release) => {
+			let resp = await client.query(`INSERT INTO test (name) VALUES('${req.body.add}')`)
+			release()
+			res.redirect('/info/get')
+		})
+	} catch (err) {
+		console.log(err)
+	}
 }
 
 exports.updateProduct = (req, res, next) => {
@@ -13,6 +36,14 @@ exports.updateProduct = (req, res, next) => {
 }
 
 exports.deleteProduct = (req, res, next) => {
-
+   try {
+        pool.connect(async (error, client, release) => {
+            let resp = await client.query(`DELETE FROM test WHERE name = ('${req.body.delete}')`)
+            release()
+            res.redirect('/product/get')
+        })
+    } catch (err) {
+        console.log(err)
+    }
 }
 
