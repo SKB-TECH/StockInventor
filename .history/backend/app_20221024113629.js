ffba6ec58adedad7f2d91bdc3c06be008f58bd4b
Shako Kinyamba <shakokinyamba201@gmail.com>
2022-10-24 11:37:45 +0200

    fix: wrong from branchs

--- .history/backend/app_20221024113629.js
+++ .history/backend/app_20221024113629.js
@@ -13,7 +13,7 @@ app.set('view engine', 'ejs');
 
 
 app.use(express.json());
-app.use(logger("dev"))
+app.use(logger(""))
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
