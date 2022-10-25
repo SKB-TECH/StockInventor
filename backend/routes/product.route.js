6f056b0ae8e0d0bd2f99b516366c5b10f7989d99
Shako Kinyamba <shakokinyamba201@gmail.com>
2022-10-25 16:40:58 +0200

    Reafact:add champs

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -7,11 +7,9 @@ const { update_product_page, add_product, getProduct, creatProduct, update_produ
 router.get('/', getProduct);
 router.post('/product/new_product', creatProduct);
 router.put('/product/update/:id', update_product);
-
 router.get('/product/addproduct', add_product);
 router.get('/product/update_product_page', update_product_page);
-
-router.get('/product/delete/:id', delete_product);
+router.delete('/product/delete/:id', delete_product);
 
 
 module.exports = router;
